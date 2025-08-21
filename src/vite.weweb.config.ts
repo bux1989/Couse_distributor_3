a7fc1e50c9d6feba39b69a535cba0f7c9c624428
bux1989 <alexfbuckle@gmail.com>
2025-08-21 22:40:37 +0200

    Update vite.weweb.config.ts

--- src/vite.weweb.config.ts
+++ src/vite.weweb.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@radix-ui/react-select@2.1.6": "@radix-ui/react-select"
+    }
+  },
   build: {
     lib: {
       entry: 'src/weweb-mount.tsx',
