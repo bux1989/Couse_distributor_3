146cd1af435e0fae1e0f128d09f79e94794f72af
bux1989 <alexfbuckle@gmail.com>
2025-08-21 22:34:58 +0200

    Update vite.weweb.config.ts

--- src/vite.weweb.config.ts
+++ src/vite.weweb.config.ts
@@ -8,10 +8,10 @@ export default defineConfig({
       entry: 'src/weweb-mount.tsx',
       formats: ['iife'],
       name: 'CourseEnrollmentBundle',
-      fileName: () => 'course-enrollment.js',
+      fileName: () => 'course-enrollment.js'
     },
     rollupOptions: {
-      external: [],
-    },
-  },
+      external: []
+    }
+  }
 });
