b56cd48eae8ccf4511b8dad4d8a391e0894897d6
bux1989 <alexfbuckle@gmail.com>
2025-08-21 22:17:24 +0200

    Update vite.config.ts

--- .weweb-check/vite.config.ts
+++ .weweb-check/vite.config.ts
@@ -1,1 +1,19 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: '../src/wwElement.vue',
+      formats: ['es'],
+      name: 'WeWebCheck',
+      fileName: () => 'weweb-check.js'
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      external: []
+    }
+  }
+});
